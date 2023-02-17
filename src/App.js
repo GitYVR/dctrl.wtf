9eae00d39eeafb5c213006471b3c8988ec2aff2e
Ubuntu <user@ubuntu.com>
2023-02-17 14:14:57 -0800

    stealthy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Grid, Link, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh", textAlign: "center" }}
+    >
+      <Grid item xs={3}>
+        <Typography variant="h4">dctrl</Typography>
+        <Typography variant="subtitle1">
+          435 w pender st, vancouver, bc
+        </Typography>
+        <br />
+        <Link href="https://discord.gg/7rjEfhtsxe">discord</Link>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
