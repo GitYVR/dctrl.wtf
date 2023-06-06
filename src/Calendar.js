d012a2e0fac21fb450775d6d38c12885214c6c68
manas <bagrimanasbir@gmail.com>
2023-06-06 14:28:16 -0700

    calendar compenonet created :D

---
+++ src/Calendar.js
@@ -0,0 +1,9 @@
+function Calender () {
+    return (
+        <div>
+            <iframe title="map" src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver" style={{border: 0}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        </div>
+    )
+}
+
+export default Calender;
