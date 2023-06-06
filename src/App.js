8b6fe4ac39e337176730f66c7f25f4d6f3fbb3ed
GitYVR <124769744+GitYVR@users.noreply.github.com>
2023-06-06 13:22:20 -0700

    Update App.js
    Added Google Calendar

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ function App() {
         </Typography>
         <br />
         <Link href="https://discord.gg/7rjEfhtsxe">discord</Link>
+        <br>
+        <br>
+        <center>
+        <iframe src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        </center>
       </Grid>
     </Grid>
   );
