d8097eb5d54e346b9a1ec4594b81fd917afbdc91
GitYVR <124769744+GitYVR@users.noreply.github.com>
2023-06-06 14:17:32 -0700

    Update App.js
    Going back to old version cuz I broke shit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Grid, Link, Typography } from "@mui/material";
+
 function App() {
   return (
     <Grid
@@ -15,9 +16,7 @@ function App() {
           435 w pender st, vancouver, bc
         </Typography>
         <br />
-        <Link href="https://discord.gg/smU8bVrDGT">discord</Link>
-        <br /> 
-        <iframe src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <Link href="https://discord.gg/7rjEfhtsxe">discord</Link>
       </Grid>
     </Grid>
   );
