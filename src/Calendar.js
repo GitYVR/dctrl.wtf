680fb70c8603ba52128a99e5760de8c8bc5eb845
manas <bagrimanasbir@gmail.com>
2023-06-07 09:59:03 -0700

    Fix App.js links and width of Calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,7 @@
 function Calender () {
     return (
         <div>
-            <iframe title="map" src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver" style={{border: 0}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe style={{width: "80vw", maxWidth: "64em" , border:0, height: "auto"}} title="map" src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver" width="800" height="600" frameborder="0" scrolling="no"></iframe>
         </div>
     )
 }
