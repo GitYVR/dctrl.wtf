23e5f808d3435a9ce7f5a2d16371659f8eba29a8
manas <bagrimanasbir@gmail.com>
2023-06-07 11:17:11 -0700

    Fix logo size and calendar a bit wider

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,7 @@
 function Calender () {
     return (
         <div style={{ width: "100%", height: "100%" }}>
-            <iframe style={{ width: "80%", maxWidth: "64em", border:0, minHeight: "512px", height: "100%", aspectRatio: 4/3 }} title="map" src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver"></iframe>
+            <iframe style={{ width: "90%", maxWidth: "64em", border:0, minHeight: "512px", height: "100%", aspectRatio: 4/3 }} title="map" src="https://calendar.google.com/calendar/embed?src=dctrlyvr%40gmail.com&ctz=America%2FVancouver"></iframe>
         </div>
     )
 }
